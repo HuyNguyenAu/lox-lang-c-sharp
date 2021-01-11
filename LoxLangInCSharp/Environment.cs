27625fd33cbd8eed135b5767eb2a4a172854672f
Huy Nguyen <HuyNguyenAu@outlook.com>
2021-01-11 21:02:54 +1100

    Working on global var support

---
+++ LoxLangInCSharp/Environment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoxLangInCSharp
+{
+    public class Environment
+    {
+        private readonly Dictionary<string, object> values = new Dictionary<string, object>();
+
+        private object Get(Token name)
+        {
+            if (values.ContainsKey(name.lexeme))
+            {
+                return values[name.lexeme];
+            }
+            throw new RuntimeError(name, $"Undefined variable '{name.lexeme}'.");
+        }
+
+        public void Define(string name, object value)
+        {
+            values.Add(name, value);
+        }
+    }
+}
